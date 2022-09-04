06486efa731218de31dc4124107910a7e52b2d14
Maverick7274 <neelanjanmukherji@gmail.com>
2022-09-04 18:58:36 +0530

    shell script added and .gitignore updated

--- gitcommands_unsecure.sh
+++ gitcommands_unsecure.sh
@@ -11,8 +11,4 @@ read -p "Please Write the Message you want to Commit : " message
 
 git commit -a -m "$message"
 
-echo "Username for 'https://github.com': Maverick7274"
-
-echo "Password for 'https://Maverick7274@github.com' : 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"
-
 git push
