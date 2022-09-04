e544f796eaa1a4efee90f7124900ab1b412fb4a6
Maverick7274 <neelanjanmukherji@gmail.com>
2022-09-04 20:03:35 +0530

    Operators added

---
+++ operators/arithmatic_operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+//  Arithmetic operators
+
+    int a = 9,b = 4, c;
+    
+    c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+    c = a*b;
+    printf("a*b = %d \n",c);
+    c = a/b;
+    printf("a/b = %d \n",c);
+    c = a%b;
+    printf("Remainder when a divided by b = %d \n",c);
+
+}
