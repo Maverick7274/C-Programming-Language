8ad85d1c9186cf8c4d69af59fe7a39fb9f00b194
Maverick7274 <neelanjanmukherji@gmail.com>
2022-09-04 18:54:52 +0530

    shell scripts updated

--- gitcommands.sh
+++ gitcommands.sh
@@ -11,8 +11,8 @@ read -p "Please Write the Message you want to Commit : " message
 
 git commit -a -m "$message"
 
-echo "Maverick7274"
+echo "Username for 'https://github.com': Maverick7274"
 
-echo "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"
+echo "Password for 'https://Maverick7274@github.com' : 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"
 
 git push
