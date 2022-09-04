8f2f82ce0ba4c6790f27b938ebb1bbd72e741c0a
Maverick7274 <neelanjanmukherji@gmail.com>
2022-09-04 18:53:17 +0530

    modified: gitcommands.sh

--- gitcommands.sh
+++ gitcommands.sh
@@ -11,8 +11,8 @@ read -p "Please Write the Message you want to Commit : " message
 
 git commit -a -m "$message"
 
+echo "Maverick7274"
+
+echo "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"
+
 git push
-
-Maverick7274
-
-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
