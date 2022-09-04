f8eb81e7c720f846a3ee8525b649e86c6828971b
Maverick7274 <neelanjanmukherji@gmail.com>
2022-09-05 01:01:50 +0530

    exercise_programs added

---
+++ escape_sequence.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("An escape sequence in C language is a sequence of characters that doesn't represent itself when used inside string literal or character. It is composed of two or more characters starting with backslash '\'. For example: '\n' represents new line.");
+    // printf("\'\a\' - Alarm or Beep");
+    // printf("\'\b\' - Backspace");
+    // printf("\'\f\' - Form Feed");
+    // printf("\'\n\' - New Line");
+    // printf("\'\r\' - Carriage Return");
+    // printf("\'\t\' - Tab (Horizontal)");
+    // printf("\'\v\' - Vertical Tab");
+    // printf("\'\\\' - Backslash");
+    // printf("\'\'\' - Single Quote");
+    // printf("\'\"\' - Double Quote");
+    // printf("\'\?\' - Question Mark");
+    // printf("\'\nnn\' - octal number");
+    // printf("'\xhh' - hexadecimal number");
+    // printf("'\0' - Null");
+
+}
