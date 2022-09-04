c6e25312217f77c8f511ffe9d9ce4cc0ca36daf1
Maverick7274 <neelanjanmukherji@gmail.com>
2022-09-04 17:34:28 +0530

    keyword.c added and README.md updated

---
+++ keywords.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("There are 32 keywords in the C Programming Language : \n");
+    printf("auto\tbreak\tcase\tchar\tconst\tcontinue\tdefault\tdo\n");
+    printf("double\telse\tenum\textern\tfloat\tfor\tgoto\tif\n");
+    printf("int\tlong\tregister\treturn\tshort\tsigned\tsizeof\tstatic\n");
+    printf("struct\tswitch\ttypedef\tunion\tunsigned\tvoid\tvolatile\twhile\n");
+    printf("");
+}
+
+/*auto	break	case	char	const	continue	default	do
+double	else	enum	extern	float	for	goto	if
+int	long	register	return	short	signed	sizeof	static
+struct	switch	typedef	union	unsigned	void	volatile	while*/
