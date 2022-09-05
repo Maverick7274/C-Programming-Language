f22485aa583188bd0935dec53e256c53f718c214
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2022-09-05 09:16:08 +0530

    added funnctions

---
+++ functions/pre_defined_func.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+// Function Initialization
+int adding();
+int subtracting();
+int multiplying();
+int dividing();
+int modulus();
+
+// Function Declaration
+adding()
+{
+    
+}
+
+
+
+int main()
+{
+    
+}
